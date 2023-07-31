f3ff1b1dc28bcab089aafa4d0883e6ea8d06f708
Milan <milan@Moniques-MacBook-Air.local>
2023-07-31 04:04:49 -0400

    Added messages functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login User
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # Register User
+        messages.error(request, 'Testing error message')
+        return redirect('accounts:register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def logout():
     return redirect('index')
