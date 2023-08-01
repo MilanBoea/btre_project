2621296172e5d6b64bfb4b548c071fef2d411b0b
Milan <milan@Moniques-Air.attlocal.net>
2023-07-31 21:44:51 -0400

    Addedcontact app

---
+++ contacts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def contact(request):
+    if request.method == 'POST':
+        print()
+        print('SUBMITTED FORM SUCCESSFULLY')
+        print()
+        
+        return
