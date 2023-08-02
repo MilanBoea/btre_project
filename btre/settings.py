8bac6368ed4c824c600b43c42ca6cba2518ea0d4
Milan <milan@Moniques-MacBook-Air.local>
2023-08-02 00:56:31 -0400

    Removed email settings from settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -147,14 +147,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'j55188319@gmail.com'
-EMAIL_HOST_PASSWORD = 'Emailforbtre5!'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
